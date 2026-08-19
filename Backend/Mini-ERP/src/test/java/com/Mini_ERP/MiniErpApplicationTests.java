91b312c584c6422d4e0e262ba228c0b76b8df6f2
Dakshata <dakshata362007@gmail.com>
2026-08-19 13:16:10 +0530

    Initialized Spring boot

---
+++ Backend/Mini-ERP/src/test/java/com/Mini_ERP/MiniErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mini_ERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
